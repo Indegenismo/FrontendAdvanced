50d707eb64a067ee6e074d4c8b9efc84d6f092e7
unknown <indegenismo@gmail.com>
2019-03-01 21:41:26 +0200

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,25 @@
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	constructor(props, state){
+		super(props,state);
+		this.state={
+			counter:0
+		}
+	}
+ render() {
+   return (
+   		<div>
+   		<button onClick={() => this.minus()}>Minus</button>
+   		<span> {''}{this.state.counter}{''}</span>
+   		<button onClick={() => this.setState({conter:this.state.counter+1})}>Plus</button>
+   		</div>
+   		)
+
+ }
+ minus(){this.setState(
+   			counter:this.state.counter - 1)};
+}
+ReactDOM.render(<App />, document.getElementById('root'));
