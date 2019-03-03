fbb7bf1ab96c6db9e8be0b5dc05b93feedea9546
unknown <indegenismo@gmail.com>
2019-03-03 11:44:26 +0200

    home work

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,89 @@
 
-import * as React from 'react';
+// import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
+// // import List from './list.js';
+
+// class Add extends React.Component {
+// 	constructor(props, state){
+// 		super(props,state);
+// 		this.state={
+// 			counter:0
+// 		};
+// 		// this.comp={
+// 		// 	term: '',
+// 		// 	items:[]
+// 		// }
+//   }	
+// //   onChange (event)  {
+// //     this.comp({term:event.target.value})
+// //   }
+// //   onSubmit  ()  {
+// //     event.preventDefault()
+// //     this.setComp({
+// //       term:'',
+// //       items:[...this.comp.items, this.comp.term]
+// //     });}
+	
+//  render() {
+//    return (
+//    		<div>
+//    		<button onClick={() => this.minus()}>Minus</button>
+//    		<span> {''}{this.state.counter}{''}</span>
+//    		<button onClick={() => this.setState({counter:this.state.counter + 1})}>Plus</button>
+// 		    {/* <div>
+// 		   <form className ="App" onSubmit={this.onSubmit}>
+// 		   <input value ={this.comp.term} onChange={this.onChange}></input>
+// 		   <button>Submit</button>
+// 		   </form>
+// 		   <list items={this.comp.items}/>
+// 	   		</div>  */}
+//    		</div>
+		  
+//    		)
+
+//  }
+ 
+//  minus(){this.setState({
+//    			counter:this.state.counter - 1})};
+// }
+// ReactDOM.render(<Add/>, document.getElementById('root'));
+
+
+import React, { Component } from 'react';
 import * as ReactDOM from 'react-dom';
+import List from './List';
 
-class App extends React.Component {
-	constructor(props, state){
-		super(props,state);
-		this.state={
-			counter:0
-		}
-	}
- render() {
-   return (
-   		<div>
-   		<button onClick={() => this.minus()}>Minus</button>
-   		<span> {''}{this.state.counter}{''}</span>
-   		<button onClick={() => this.setState({counter:this.state.counter + 1})}>Plus</button>
-   		</div>
-   		)
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      items: []
+    };
+  }
 
- }
- minus(){this.setState({
-   			counter:this.state.counter - 1})};
+  onChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  onSubmit  (event)  {
+    event.preventDefault();
+    this.setState({
+      term: '',
+      items: [...this.state.items, this.state.term]
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>Submit</button>
+        </form>
+        <List items={this.state.items} />
+      </div>
+    );
+  }
 }
 ReactDOM.render(<App/>, document.getElementById('root'));
